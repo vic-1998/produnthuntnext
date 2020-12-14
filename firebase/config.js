8ad6e8b96910dd4ce79e21770839263e6abeeecf
Victor Manuel Plaza <69982635+vic-1998@users.noreply.github.com>
2020-12-14 11:31:06 -0500

    produnthuntnext

---
+++ firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC5T896HMPSTWI0o9f9nlzjih9XJvdJE7Q",
+    authDomain: "produnthuntclone.firebaseapp.com",
+    databaseURL: "https://produnthuntclone-default-rtdb.firebaseio.com",
+    projectId: "produnthuntclone",
+    storageBucket: "produnthuntclone.appspot.com",
+    messagingSenderId: "377617196710",
+    appId: "1:377617196710:web:0aa7761b0919d5b5f4b667"
+};
+
+export default firebaseConfig;
